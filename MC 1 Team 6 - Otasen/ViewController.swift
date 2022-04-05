162c39de479c2ea37fd756a0433eae9669131620
Ubean <76271600+ubeann@users.noreply.github.com>
2022-04-05 10:02:08 +0700

    Initial Commit

---
+++ MC 1 Team 6 - Otasen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MC 1 Team 6 - Otasen
+//
+//  Created by Muhammad Rizal Bagus Prakasa on 05/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
